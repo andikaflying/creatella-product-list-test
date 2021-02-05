1387c1ff95567679a16201d2b09aebde2f6c8bb1
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-05 18:49:06 +0700

    display product error

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -6,14 +6,21 @@ const initialState = {
     data: []
 };
 
-const DISPLAY_PRODUCT = "DISPLAY_PRODUCT"
+const DISPLAY_PRODUCT_SUCCESS = "Display Product Success"
+const DISPLAY_PRODUCT_FAILURE = "Display Product Failure"
+const ERROR_MESSAGE = "Server error"
 
 const productReducer = (state, action) => {
   switch (action.type) {
-    case DISPLAY_PRODUCT:
+    case DISPLAY_PRODUCT_SUCCESS:
       return {
         ...state,
         data: action.payload
+      };
+    case DISPLAY_PRODUCT_FAILURE: 
+      return {
+        ...state,
+        error: action.error
       };
     default:
       return state;
@@ -24,18 +31,16 @@ export function useGetProducts() {
     const [data, dispatch] = useReducer(productReducer, initialState);
   
     useEffect(() => {
-      // dispatch(displayProductSuccess());
-  
       fetch("https://api.mocki.io/v1/a135f562")
         .then(response => {
           if (!response.ok) throw Error(response.statusText);
           return response.json();
         })
         .then(json => {
-          dispatch({ type: DISPLAY_PRODUCT, payload: json });
+          dispatch({ type: DISPLAY_PRODUCT_SUCCESS, payload: json });
         })
         .catch(error => {
-          // dispatch(displayProductFailure({ payload: error.message }));
+          dispatch({ type: DISPLAY_PRODUCT_FAILURE, error : ERROR_MESSAGE})
         });
     }, []);
     
