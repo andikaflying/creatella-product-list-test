0c8326e57ac9dd6bbeb6707ba1cbf462574e43f8
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-06 23:19:29 +0700

    sorting

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import ProductGrid from './ProductGrid';
-import * as ReducerActions from "../reducers/reducer";
+import { initialState, productReducer, DISPLAY_PRODUCT_SUCCESS, DISPLAY_PRODUCT_FAILURE,
+         SORT_PRODUCT_SUCCESS, SORT_PRODUCT_FAILURE, ERROR_MESSAGE } from "../reducers/reducer";
+import { useEffect, useState, useReducer } from "react";
 
 function ProductPage() {
-    const products = ReducerActions.useGetProducts();
-    console.log("data displayed = " + JSON.stringify(products))   
+    const [products, dispatch] = useReducer(productReducer, initialState);
+
+    const sorting = (e) => {
+        fetch("https://api.mocki.io/v1/a135f562")
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                    return response.json();
+                }
+            )
+            .then(json => {
+                dispatch({ type: SORT_PRODUCT_SUCCESS, payload: json });
+            })
+            .catch(error => {
+                dispatch({ type: SORT_PRODUCT_FAILURE, error : ERROR_MESSAGE})
+            }); 
+    }
+
+    useEffect(() => {
+        fetch("https://api.mocki.io/v1/a135f562")
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                    return response.json();
+                }
+            )
+            .then(json => {
+                dispatch({ type: DISPLAY_PRODUCT_SUCCESS, payload: json });
+            })
+            .catch(error => {
+                dispatch({ type: DISPLAY_PRODUCT_FAILURE, error : ERROR_MESSAGE})
+            }); 
+    }, []);
+
     return(
         <div class="container">
-            { (products != null) && <ProductGrid products={products} /> }
+            <div class="select">
+                <select name="slct" id="slct" onChange={sorting} >
+                    <option selected disabled>Sorted by .... </option>
+                    <option value="size">Size</option>
+                    <option value="price">Price</option>
+                    <option value="id">ID</option>
+                </select>
+            </div>
+            { (products != null) && <ProductGrid products={products.data} /> }
         </div>
     )
 };
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,20 +1,24 @@
 import { useEffect, useReducer } from "react";
 
-const initialState = {
+export const initialState = {
     loading: "",
     error: "",
+    firstInit: false,
     data: []
 };
 
-const DISPLAY_PRODUCT_SUCCESS = "Display Product Success"
-const DISPLAY_PRODUCT_FAILURE = "Display Product Failure"
-const ERROR_MESSAGE = "Server error"
+export const DISPLAY_PRODUCT_SUCCESS = "Display Product Success"
+export const DISPLAY_PRODUCT_FAILURE = "Display Product Failure"
+export const SORT_PRODUCT_SUCCESS = "Sort Product Success"
+export const SORT_PRODUCT_FAILURE = "Sort Product Failure"
+export const ERROR_MESSAGE = "Server error"
 
-const productReducer = (state, action) => {
+export const productReducer = (state, action) => {
   switch (action.type) {
     case DISPLAY_PRODUCT_SUCCESS:
       return {
         ...state,
+        firstInit: true,
         data: action.payload
       };
     case DISPLAY_PRODUCT_FAILURE: 
@@ -22,27 +26,58 @@ const productReducer = (state, action) => {
         ...state,
         error: action.error
       };
+    case SORT_PRODUCT_SUCCESS:
+        return {
+          ...state,
+          data: action.payload
+        };
+    case SORT_PRODUCT_FAILURE: 
+        return {
+          ...state,
+          error: action.error
+        };  
     default:
       return state;
   }
 };
 
-export function useGetProducts() {
-    const [data, dispatch] = useReducer(productReducer, initialState);
-  
-    useEffect(() => {
-      fetch("https://api.mocki.io/v1/a135f562")
-        .then(response => {
-          if (!response.ok) throw Error(response.statusText);
-          return response.json();
-        })
-        .then(json => {
-          dispatch({ type: DISPLAY_PRODUCT_SUCCESS, payload: json });
-        })
-        .catch(error => {
-          dispatch({ type: DISPLAY_PRODUCT_FAILURE, error : ERROR_MESSAGE})
-        });
-    }, []);
+// export function useGetProducts() {
+//     fetch("https://api.mocki.io/v1/a135f562")
+//       .then(response => {
+//           if (!response.ok) throw Error(response.statusText);
+//           return response.json();
+//       })
+//         .then(json => {
+//           dispatch({ type: DISPLAY_PRODUCT_SUCCESS, payload: json });
+//         })
+//         .catch(error => {
+//           dispatch({ type: DISPLAY_PRODUCT_FAILURE, error : ERROR_MESSAGE})
+//         });
+//     return data.data;
+// }
+
+// export function useSortProducts(type) {
+//     const [data, dispatch] = useReducer(productReducer, initialState);
+//     console.log("Type = " + type);
+
+//     useEffect(() => {
+
+//       console.log("stress")
+//       // if (type != null) {
+//         console.log("Masuk if. type = " + type)
+//         fetch("https://api.mocki.io/v1/a135f562")
+//         .then(response => {
+//           if (!response.ok) throw Error(response.statusText);
+//           return response.json();
+//         })
+//         .then(json => {
+//           dispatch({ type: SORT_PRODUCT_SUCCESS, payload: json });
+//         })
+//         .catch(error => {
+//           dispatch({ type: SORT_PRODUCT_FAILURE, error : ERROR_MESSAGE})
+//         });
+//       // }
+//     }, []);
     
-    return data.data;
-}
+//     return data.data;
+// }
