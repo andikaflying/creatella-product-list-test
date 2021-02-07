52d89f1ae34b1c4d39a99f95d1159e3987754ad5
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-08 02:13:41 +0700

    infinite scroll

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProductGrid from './ProductGrid';
 import { initialState, productReducer, DISPLAY_PRODUCT_SUCCESS, DISPLAY_PRODUCT_FAILURE,
-         SORT_PRODUCT_SUCCESS, SORT_PRODUCT_FAILURE, ERROR_MESSAGE } from "../reducers/reducer";
+         SORT_PRODUCT_SUCCESS, SORT_PRODUCT_FAILURE, ERROR_MESSAGE, 
+         DISPLAY_PRODUCT_OTHER_PAGE_SUCCESS, DISPLAY_PRODUCT_OTHER_PAGE_FAILURE } from "../reducers/reducer";
 import { useEffect, useState, useReducer } from "react";
+import useInfiniteScroll from '../utilities/useInfiniteScroll';
 
 function ProductPage() {
     const [products, dispatch] = useReducer(productReducer, initialState);
+    const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreProduct);
+
 
     const sorting = (e) => {
         fetch("https://api.mocki.io/v1/a135f562")
@@ -37,6 +41,25 @@ function ProductPage() {
             }); 
     }, []);
 
+    function fetchMoreProduct() {
+        setTimeout(() => {
+          console.log("fetch more product") 
+          fetch("https://api.mocki.io/v1/a135f562")
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                    return response.json();
+                }
+            )
+            .then(json => {
+                dispatch({ type: DISPLAY_PRODUCT_OTHER_PAGE_SUCCESS, payload: json });
+            })
+            .catch(error => {
+                dispatch({ type: DISPLAY_PRODUCT_OTHER_PAGE_FAILURE, error : ERROR_MESSAGE})
+            }); 
+          setIsFetching(false);
+        }, 1000);
+    }
+
     return(
         <div class="container">
             <div class="select">
@@ -48,6 +71,7 @@ function ProductPage() {
                 </select>
             </div>
             { (products != null) && <ProductGrid products={products.data} /> }
+            {isFetching && 'Fetching more list items...' }
         </div>
     )
 };
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -3,7 +3,6 @@ import { useEffect, useReducer } from "react";
 export const initialState = {
     loading: "",
     error: "",
-    firstInit: false,
     data: []
 };
 
@@ -11,6 +10,8 @@ export const DISPLAY_PRODUCT_SUCCESS = "Display Product Success"
 export const DISPLAY_PRODUCT_FAILURE = "Display Product Failure"
 export const SORT_PRODUCT_SUCCESS = "Sort Product Success"
 export const SORT_PRODUCT_FAILURE = "Sort Product Failure"
+export const DISPLAY_PRODUCT_OTHER_PAGE_SUCCESS = "Display Product Some Page Success"
+export const DISPLAY_PRODUCT_OTHER_PAGE_FAILURE = "Display Product Some Page Failure"
 export const ERROR_MESSAGE = "Server error"
 
 export const productReducer = (state, action) => {
@@ -18,7 +19,6 @@ export const productReducer = (state, action) => {
     case DISPLAY_PRODUCT_SUCCESS:
       return {
         ...state,
-        firstInit: true,
         data: action.payload
       };
     case DISPLAY_PRODUCT_FAILURE: 
@@ -32,6 +32,14 @@ export const productReducer = (state, action) => {
           data: action.payload
         };
     case SORT_PRODUCT_FAILURE: 
+        return {
+          ...state,
+          error: action.error
+        };  
+    case DISPLAY_PRODUCT_OTHER_PAGE_SUCCESS:
+        state.data.push(action.payload)
+        return state;
+    case DISPLAY_PRODUCT_OTHER_PAGE_FAILURE: 
         return {
           ...state,
           error: action.error
