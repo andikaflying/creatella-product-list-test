1b215300570efed594220afad1dd72536f40aee1
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-08 23:34:46 +0700

    remove unnecessary ad comment, generate random ad index

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,32 +1,16 @@
 import { useEffect, useState, useReducer } from "react";
 import ProductPage from '../components/ProductPage';
-import { generateAdIndex, useLocalStorage, AD_IMAGE_URL } from '../utilities/GeneralUtils';
 
 const Main = () => {
-    const [addIndexList, setAddIndexList] = useLocalStorage('adList', new Array());
-    const [adURL, setAdURL] = useState("");
-
-    useEffect(() => {
-        console.log("Index list first = " + JSON.stringify(addIndexList))
-        const index = generateAdIndex(addIndexList)
-        const newList = [...addIndexList, index]
-
-        setAddIndexList(newList)
-        setAdURL(AD_IMAGE_URL + index)
-    }, []);
-
     return(
         <>
             <header>
                 <h1>Products Grid</h1>
     
                 <p>Here you're sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
-    
-                <p>But first, a word from our sponsors:</p> 
-                <img class = "ad" src={adURL} />
             </header>
             <section class="products">
-                {/* <ProductPage /> */}
+                <ProductPage />
             </section>
         </>
     )
--- src/utilities/GeneralUtils.js
+++ src/utilities/GeneralUtils.js
@@ -1,50 +1,26 @@
 import { useState } from "react";
 
-export function generateAdIndex(adIndexList) {
-    let isDone = false;
-    let index;
-    while (!isDone) {
-        index = Math.floor(Math.random()*1000);
-        if (!adIndexList.includes(index)) {
-            isDone = true;
-        }
-    }
+const adIndexList = new Array();
 
-    return index;
+export function generateAd() {
+  let isDone = false;
+  let index;
+
+  while (!isDone) {
+      index = Math.floor(Math.random()*1000);
+      if (!adIndexList.includes(index)) {
+          isDone = true;
+      }
+  }
+
+  adIndexList.push(index)
+
+  return AD_IMAGE_URL + index;
 }
 
-const LIMIT = 15
+export const LIMIT = 20
 
 export const BASE_URL = "http://localhost:3000/"
 export const ENDPOINT_DISPLAY_PRODUCT = BASE_URL + "api/products?_limit=" + LIMIT + "&page="
 export const ENDPOINT_SORT_PRODUCT = BASE_URL + "api/products?_sort="
-export const ENDPOINT_DISPLAY_AD = BASE_URL + ""
 export const AD_IMAGE_URL = BASE_URL + "ads/?r="
-
-// export const ENDPOINT_DISPLAY_PRODUCT = "https://api.mocki.io/v1/a135f562"
-// export const ENDPOINT_SORT_PRODUCT = "https://api.mocki.io/v1/a135f562"
-// export const ENDPOINT_DISPLAY_AD = "https://api.mocki.io/v1/a135f562"
-
-export function useLocalStorage(key, initialValue) {
-    const [storedValue, setStoredValue] = useState(() => {
-      try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
-      } catch (error) {
-        console.log(error);
-        return initialValue;
-      }
-    });
-  
-    const setValue = value => {
-      try {
-        const valueToStore = value instanceof Function ? value(storedValue) : value;
-        setStoredValue(valueToStore);
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    return [storedValue, setValue];
-}
